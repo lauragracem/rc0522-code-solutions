5fb290efab1c8aacbda9b27feef4c21b7cdd3f5f
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-18 20:17:53 +0000

    edit javascript-if

--- javascript-if/main.js
+++ javascript-if/main.js
@@ -54,7 +54,7 @@ function isOldEnoughToDrinkAndDrive(person) {
 function categorizeAcidity(pH) {
   if (pH === 7) {
     return 'neutral';
-  } else if (pH < 7) {
+  } else if (pH < 7 & pH >= 0) {
     return 'acid';
   } else if (pH > 7 & pH <= 14) {
     return 'base';
