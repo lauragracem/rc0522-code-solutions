85651ce6b6ec6c114252442ca4c4982bace6e248
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-23 21:55:46 +0000

    labels added

--- dom-events/buttons.js
+++ dom-events/buttons.js
@@ -1,29 +1,26 @@
 function handleclick(event) {
   console.log('button clicked');
-  console.log(event);
-  console.log(event.target);
+  console.log('event:', event);
+  console.log('event.target:', event.target);
 }
 
 var $clickMe = document.querySelector('.click-button');
 $clickMe.addEventListener('click', handleclick);
-console.log($clickMe);
 
 function handleMouseover(event) {
   console.log('Hover Me!');
-  console.log(event);
-  console.log(event.target);
+  console.log('event:', event);
+  console.log('event.target:', event.target);
 }
 
 var $mouseOver = document.querySelector('.hover-button');
 $mouseOver.addEventListener('mouseover', handleMouseover);
-console.log($mouseOver);
 
 function handleDoubleClick(event) {
   console.log('Double Click Me!');
-  console.log(event);
-  console.log(event.target);
+  console.log('event:', event);
+  console.log('event.target:', event.target);
 }
 
 var $doubleClick = document.querySelector('.double-click-button');
 $doubleClick.addEventListener('dblclick', handleDoubleClick);
-console.log($doubleClick);
