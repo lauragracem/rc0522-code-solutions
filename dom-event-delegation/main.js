b5ec25cbbc4b22f59b5360e658ec6dda9daf51e9
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-26 18:42:45 +0000

    if edit

--- dom-event-delegation/main.js
+++ dom-event-delegation/main.js
@@ -4,7 +4,10 @@ $taskClick.addEventListener('click', clickDone);
 function clickDone(event) {
   console.log('event.target: ', event.target);
   console.log('event.target.tagName: ', event.target.tagName);
-  console.log('event.target.closest:', event.target.closest('.task-list-item'));
-  var closest = event.target.closest('.task-list-item');
-  closest.remove();
+
+  if (event.target.tagName === 'BUTTON') {
+    console.log('event.target.closest:', event.target.closest('.task-list-item'));
+    var closest = event.target.closest('.task-list-item');
+    closest.remove();
+  }
 }
