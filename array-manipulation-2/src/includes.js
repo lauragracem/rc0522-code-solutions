dadb224df2fa51c5f78e0678160317cac8b360fd
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-28 00:29:27 +0000

    includes

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,10 @@
 /* exported includes */
+function includes(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    var index = array[i];
+    if (value === index) {
+      return true;
+    }
+  }
+  return false;
+}
