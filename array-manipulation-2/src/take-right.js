e913441f0756fb6fc11e3980648713489d02d185
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-28 03:56:15 +0000

    take right

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,16 @@
 /* exported takeRight */
+
+function takeRight(array, count) {
+  var newArray = [];
+  if (count > array.length) {
+    return array;
+  }
+  for (var i = array.length - count; i < array.length; i++) {
+    var index = array[i];
+    newArray.push(index);
+  }
+  return newArray;
+}
+
+// count = the number of items from the end of the array
+// returned in the order it was inputted.
