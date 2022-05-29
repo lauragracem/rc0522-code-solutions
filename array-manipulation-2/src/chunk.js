b6069321f47048a635cb41d26af076d9b0589113
Laura Grace Manimtim <lauragracem@gmail.com>
2022-05-29 03:09:15 +0000

    added chunk

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,4 +1,11 @@
 /* exported chunk */
 function chunk(array, size) {
+  var newArray = [];
 
+  for (var i = 0; i < array.length; i += size) {
+    var result = array.slice(i, i + size);
+    newArray.push(result);
+  }
+
+  return newArray;
 }
