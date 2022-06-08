28d60c177cb8dd2342eb73d2886ab307a6b7bbb8
Laura Grace Manimtim <lauragracem@gmail.com>
2022-06-08 23:01:36 +0000

    progress dots working

--- javascript-carousel/main.js
+++ javascript-carousel/main.js
@@ -5,6 +5,57 @@ var $fourth = document.querySelector('.fourth');
 var $fifth = document.querySelector('.fifth');
 var $leftArrow = document.querySelector('.fa-solid.fa-angle-left');
 var $rightArrow = document.querySelector('.fa-solid.fa-angle-right');
+var $dot1 = document.querySelector('#first');
+var $dot2 = document.querySelector('#second');
+var $dot3 = document.querySelector('#third');
+var $dot4 = document.querySelector('#fourth');
+var $dot5 = document.querySelector('#fifth');
+
+$dot1.addEventListener('click', dotOne);
+function dotOne() {
+  $first.className = 'first';
+  $second.className = 'second hidden';
+  $third.className = 'third hidden';
+  $fourth.className = 'fourth hidden';
+  $fifth.className = 'fifth hidden';
+}
+
+$dot2.addEventListener('click', dotTwo);
+function dotTwo() {
+  $first.className = 'first hidden';
+  $second.className = 'second';
+  $third.className = 'third hidden';
+  $fourth.className = 'fourth hidden';
+  $fifth.className = 'fifth hidden';
+
+}
+
+$dot3.addEventListener('click', dotThree);
+function dotThree() {
+  $first.className = 'first hidden';
+  $second.className = 'second hidden';
+  $third.className = 'third';
+  $fourth.className = 'fourth hidden';
+  $fifth.className = 'fifth hidden';
+}
+
+$dot4.addEventListener('click', dotFourth);
+function dotFourth() {
+  $first.className = 'first hidden';
+  $second.className = 'second hidden';
+  $third.className = 'third hidden';
+  $fourth.className = 'fourth';
+  $fifth.className = 'fifth hidden';
+}
+
+$dot5.addEventListener('click', dotFifth);
+function dotFifth() {
+  $first.className = 'first hidden';
+  $second.className = 'second hidden';
+  $third.className = 'third hidden';
+  $fourth.className = 'fourth hidden';
+  $fifth.className = 'fifth';
+}
 
 $leftArrow.addEventListener('click', goBack);
 function goBack() {
@@ -61,31 +112,3 @@ function goForward() {
     $first.className = 'first';
   }
 }
-
-// function spin() {
-//   if ($first === '.first') {
-//
-//   }
-//   if ($second === '.second.hidden') {
-//
-//   }
-//   $images++;
-//
-//
-// $third.className = 'third';
-// $fourth.className = 'fourth';
-// $fifth.className = 'fifth';
-
-// var timer = setInterval(countDown, 1000);
-
-// var $h1 = document.querySelector('h1');
-
-// function countDown() {
-//   if ($h1.textContent === '1') {
-//     $h1.textContent = '~Earth Beeeelooowww Us~';
-//     clearInterval(timer);
-//     return;
-//   }
-//   $h1.textContent--;
-
-// }
