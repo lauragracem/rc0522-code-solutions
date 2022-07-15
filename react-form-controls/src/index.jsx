ed944efa3abcf3d8a95b38d83b93d6f4ce88851b
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-15 22:02:52 +0000

    Methods defined

---
+++ react-form-controls/src/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class RegistrationForm extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+}
+
+render(){
+  return(
+    <form>
+      <button></button>
+      <input></input>
+      <input></input>
+    </form>
+  )
+}
