5f9ed708ced86a7c193203f83acb00656b041606
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-15 03:08:43 +0000

    Setup Complete

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class CustomButton extends React.Component {
+  render() {
+    return <button>Click Me!</button>;
+  }
+}
+
+const click = document.querySelector('#root');
