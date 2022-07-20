46263000e4bb7e68d6e46444bc8242fedb642d66
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-20 23:54:56 +0000

    Edit - line 17 removed

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -14,7 +14,6 @@ class App extends React.Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {
-        this.setState({ users });
         this.setState({ isLoading: false });
       });
   }
