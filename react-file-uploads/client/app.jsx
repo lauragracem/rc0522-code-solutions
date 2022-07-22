ef4a66bcd4b6458bf90c390b73dbb1d367ff44af
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-22 19:22:03 +0000

    Complete the Client In Progress

--- react-file-uploads/client/app.jsx
+++ react-file-uploads/client/app.jsx
@@ -16,6 +16,26 @@ export default class App extends React.Component {
   }
 
   handleSubmit(event) {
+    event.preventdefault();
+    const formData = new FormData();
+    const caption = this.state.caption;
+    const image = this.fileInputRef.current.files[0];
+
+    formData.append('caption', caption);
+    formData.append('image', image);
+
+    fetch('/api/uploads', {
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        this.setState({ caption: '' });
+        this.fileInputRef.current.value = null;
+      })
+      .catch(err => console.log(err));
+
     /**
      * Prevent the browser's default behavior for form submissions.
      *
