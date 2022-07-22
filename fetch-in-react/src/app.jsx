a2748a669c7b1a09439d3c13a480403e0474fc3c
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-22 22:09:39 +0000

    Line 17 edited and code update tested

--- fetch-in-react/src/app.jsx
+++ fetch-in-react/src/app.jsx
@@ -14,7 +14,7 @@ class App extends React.Component {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => {
-        this.setState({ isLoading: false });
+        this.setState({ users, isLoading: false });
       });
   }
 
