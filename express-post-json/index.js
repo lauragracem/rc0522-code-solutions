eeea6cd92bce888e0f2732594e8d301d30a278fb
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-23 16:48:16 +0000

    Completed express-post-json

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const app = express();
 
-// const nextID = 1;
+let nextId = 1;
 const grades = {};
 
 app.get('/api/grades', (req, res) => {
-  res.json({ grades });
+  const gradesArray = [];
+  for (const id in grades) {
+    gradesArray.push(grades[id]);
+  }
+  res.json(gradesArray);
 });
 
 app.use(express.json());
 
 app.post('/api/grades', (req, res) => {
-  res.sendStatus(201);
+  const { name, course, score } = req.body;
+  const grade = { id: nextId, name, course, score };
+  grades[nextId] = grade;
+  nextId++;
+  res.status(201).json(grade);
 });
 
 app.listen(3000, () => {
-  // console.log('Listening on port 3000!');
+  console.log('Listening on port 3000!');
 });
