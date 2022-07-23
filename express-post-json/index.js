fba298eb1bb2557b345f210f691763ab8fb2616a
Laura Grace Manimtim <lauragracem@gmail.com>
2022-07-23 05:55:30 +0000

    In Progress

--- express-post-json/index.js
+++ express-post-json/index.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const app = express();
 
-const nextID = 1;
+// const nextID = 1;
 const grades = {};
-
-// app.METHOD(PATH, HANDLER);
 
 app.get('/api/grades', (req, res) => {
   res.json({ grades });
 });
 
+app.use(express.json());
+
+app.post('/api/grades', (req, res) => {
+  res.sendStatus(201);
+});
+
 app.listen(3000, () => {
-  console.log('Listening on port 3000!');
+  // console.log('Listening on port 3000!');
 });
