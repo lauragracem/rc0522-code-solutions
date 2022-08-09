ec4f453887d45ac7900f1fb173354343370ae1c1
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 20:14:17 +0000

    Completed insertUnderTop(stack,value)

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -11,9 +11,3 @@ function get2ndFromTop(stack) {
 
   return secondTop;
 }
-
-// pop the "top" and save it into a variable
-// check if top is undefined, if it is undefined return
-// Use peek to get the second item from the stack, save in variable
-// Push the top back on
-// Return the variable that holds the second item
--- data-structures-stacks/insert-under-top.js
+++ data-structures-stacks/insert-under-top.js
@@ -1,5 +1,13 @@
 /* exported insertUnderTop */
 
 function insertUnderTop(stack, value) {
+  const popTop = stack.pop();
+  if (popTop === undefined) {
+    return;
+  }
 
+  stack.push(value);
+  stack.push(popTop);
+
+  return stack;
 }
