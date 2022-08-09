b229b500a07dd2849aa9ca02c0857c17b7d63001
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 21:42:42 +0000

    Completed postpone(queue)

--- data-structures-queues/postpone.js
+++ data-structures-queues/postpone.js
@@ -1,5 +1,10 @@
 /* exported postpone */
 
 function postpone(queue) {
-
+  const move = queue.dequeue();
+  if (move === undefined) {
+    return;
+  }
+  const putBack = queue.enqueue(move);
+  return putBack;
 }
