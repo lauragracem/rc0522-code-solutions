99aa1b6e66eefc222215e067d75dce4bdef170fe
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 20:56:40 +0000

    Completed maxValue(stack)

--- data-structures-stacks/max-value.js
+++ data-structures-stacks/max-value.js
@@ -1,5 +1,11 @@
 /* exported maxValue */
 
 function maxValue(stack) {
-
+  let maxValue = -Infinity;
+  let popped = stack.pop();
+  while (popped !== undefined) {
+    maxValue = Math.max(maxValue, popped);
+    popped = stack.pop();
+  }
+  return maxValue;
 }
