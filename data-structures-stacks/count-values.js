a8848de9158df337a28cf82016bd7699bf324ad3
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 20:38:24 +0000

    Completed countValues(stack)

--- data-structures-stacks/count-values.js
+++ data-structures-stacks/count-values.js
@@ -1,4 +1,15 @@
 /* exported countValues */
 
 function countValues(stack) {
+  const values = [];
+  let popped = stack.pop();
+  while (popped !== undefined) {
+    values.push(popped);
+    popped = stack.pop();
+  }
+  for (let i = values.length - 1; i > 0; i--) {
+    stack.push(values[i]);
+  }
+  return values.length;
+
 }
