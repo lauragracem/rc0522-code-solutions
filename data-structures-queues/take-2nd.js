8c65aa69c4f4144ab8110631a35fc84b39a2a429
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 23:51:36 +0000

    Completed takeSmaller(queue)

--- data-structures-queues/take-2nd.js
+++ data-structures-queues/take-2nd.js
@@ -1,5 +1,12 @@
 /* exported take2nd */
 
 function take2nd(queue) {
+  const remove = queue.dequeue();
+  if (remove === undefined) {
+    return;
+  }
+  if (remove !== undefined) {
+    return remove;
+  }
 
 }
--- data-structures-queues/take-smaller.js
+++ data-structures-queues/take-smaller.js
@@ -1,5 +1,27 @@
 /* exported takeSmaller */
 
 function takeSmaller(queue) {
+  const get = queue.dequeue();
+  if (get === undefined) {
+    return get;
+  }
+  const get2 = queue.dequeue();
+  if (get2 === undefined) {
+    return get;
+  }
 
+  if (get > get2) {
+    queue.enqueue(get);
+    return get2;
+  } else {
+    queue.enqueue(get2);
+    return get;
+  }
 }
+// Get first item from queue
+// If first item is undefined, return
+// Get second item from queue
+// If second item is undefined, return first item
+
+// If first item is greater than second item, enqueue first item, return second item
+// Else enqueue second item and return first item
