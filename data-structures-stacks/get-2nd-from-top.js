1cb8dcea6ab553df85107c9b1f8bce38ad8e0798
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-09 20:01:13 +0000

    Completed get2ndFromTop(stack)

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,10 +1,19 @@
 /* exported get2ndFromTop */
 
 function get2ndFromTop(stack) {
-  // return stack.peek();
-  // const value1 = stack.pop();
-  // const value2 = stack.pop();
-  // stack.push(value2);
-  // stack.push(value1);
-  // return value2;
+  const popTop = stack.pop();
+  if (popTop === undefined) {
+    return;
+  }
+
+  const secondTop = stack.peek();
+  stack.push(popTop);
+
+  return secondTop;
 }
+
+// pop the "top" and save it into a variable
+// check if top is undefined, if it is undefined return
+// Use peek to get the second item from the stack, save in variable
+// Push the top back on
+// Return the variable that holds the second item
