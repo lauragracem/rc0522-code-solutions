81accb0efbf47b507c9df10b76c8c312d4281a14
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-10 15:42:23 +0000

    Completed takeNextSmallest(queue)

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,25 +1,22 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
-  let prev = queue.dequeue();
-  if (prev === undefined) {
+  let current = queue.dequeue();
+  if (current === undefined) {
     return;
   }
 
-  // peek at next
   let next = queue.peek();
   while (next !== undefined) {
-    if (prev > next) {
-      queue.enqueue(prev);
-      prev = next;
+    if (current > next) {
+      queue.enqueue(current);
+      current = next;
       queue.dequeue();
       next = queue.peek();
     } else {
-      queue.dequeue();
-      queue.enqueue(next);
-      return prev;
+      return current;
     }
   }
 
-  return prev;
+  return current;
 }
