e5b952ed3a5673f3967918f9d2d9725331862c94
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-10 01:45:05 +0000

    in progress

--- data-structures-queues/take-next-smallest.js
+++ data-structures-queues/take-next-smallest.js
@@ -1,5 +1,25 @@
 /* exported takeNextSmallest */
 
 function takeNextSmallest(queue) {
+  let prev = queue.dequeue();
+  if (prev === undefined) {
+    return;
+  }
 
+  // peek at next
+  let next = queue.peek();
+  while (next !== undefined) {
+    if (prev > next) {
+      queue.enqueue(prev);
+      prev = next;
+      queue.dequeue();
+      next = queue.peek();
+    } else {
+      queue.dequeue();
+      queue.enqueue(next);
+      return prev;
+    }
+  }
+
+  return prev;
 }
