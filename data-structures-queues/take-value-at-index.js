0586ca17c1d569f30e876be287fb78d211c74916
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-10 00:43:42 +0000

    Completed takeValueAtIndex(queue,index)

--- data-structures-queues/take-value-at-index.js
+++ data-structures-queues/take-value-at-index.js
@@ -1,5 +1,14 @@
 /* exported takeValueAtIndex */
 
 function takeValueAtIndex(queue, index) {
+  let value = queue.dequeue();
+  if (value === undefined) {
+    return;
+  }
 
+  for (let i = 0; i < index; i++) {
+    queue.enqueue(value);
+    value = queue.dequeue();
+  }
+  return value;
 }
