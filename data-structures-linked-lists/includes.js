7e272173fcb302e5e6f16b3bfb8cd678c8b88ea4
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-11 21:17:14 +0000

    Completed includes(list,value)

--- data-structures-linked-lists/includes.js
+++ data-structures-linked-lists/includes.js
@@ -1,9 +1,11 @@
 /* exported includes */
 
 function includes(list, value) {
-  // while (list !== null) {
-  //   if(list.data === value) return true;
-  //   list = list.next;
-  // }
-  // return false;
+  while (list !== null) {
+    if (list.data === value) {
+      return true;
+    }
+    list = list.next;
+  }
+  return false;
 }
