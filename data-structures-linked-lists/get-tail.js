2ffe790367f593d492801960230dccc8767d3558
Laura Grace Manimtim <lauragracem@gmail.com>
2022-08-11 20:52:21 +0000

    Completed getTail(list)

--- data-structures-linked-lists/get-tail.js
+++ data-structures-linked-lists/get-tail.js
@@ -1,8 +1,8 @@
 /* exported getTail */
 
 function getTail(list) {
-  // while (list.next) {
-  // list = list.next;
-  // }
-  // return list.data;
+  while (list.next) {
+    list = list.next;
+  }
+  return list.data;
 }
